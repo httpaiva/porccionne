d5c1fe2b18a5d4f3f1e7ce47d7f36bcc701c1cdb
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-07-25 19:36:40 -0300

    Aplicando estilo oficial ao frontend

--- pages/index.js
+++ pages/index.js
@@ -4,47 +4,49 @@ import Head from 'next/head';
 export default function Home() {
 
   return (
-    <div className="container">
-      <Head>
-        <title>Porccionne!</title>
-      </Head>
-
-      <main>
-        <div className="header">
-          <h1>Bem-vindo ao Porccionne!</h1>
-          <p>
-            Porccione é a sua calculadora de porções online.
-          </p>
-          <p>
-            Aqui você pode planejar aquele seu churrasco do fim de semana ou calcular a receita de bolo para toda a sua família.
-          </p>
-        </div>
-        <div className="form">
-          <label>Cole sua receita aqui:</label>
-          <br />
-          <textarea
-            name="textarea"
-            id="textarea"
-            cols="100"
-            rows="20"
+    <div className="wrapper">
+      <div className="container">
+        <Head>
+          <title>Porccionnne!</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="keywords" content="porccionne, calculadora, receitas, porções" />
+          <meta
+            name="description"
+            content="Porccione é a sua calculadora de porções de receitas. 
+            Aqui você pode planejar aquele seu churrasco do fim de semana 
+            ou calcular a receita de bolo para toda a sua família."
           />
-          <br />
-          <label>Quantas porções essa receita rende:</label>
-          <br />
-          <input type="number" name="item" id="item" min="1" max="1000" />
-          <br />
-          <label>Quantas porções você deseja fazer render:</label>
-          <br />
-          <input type="number" name="item" id="item" min="1" max="1000" />
-          <br />
-          <button type="submit">Calcular</button>
-
-        </div >
-      </main >
-
-      <footer>
-        
+          <meta name="robots" content="index, follow" />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></link>
+        </Head>
+        <header className="header">
+          <img className="logo" src={require('../img/logo.svg')} alt="Porccionne Logo" />
+          <h1 className="subtitulo">Produza suas receitas conosco</h1>
+        </header>
+        <main>
+          <div className="inicial">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque dui vitae amet tortor. Imperdiet auctor faucibus aliquam etiam eu in tellus sit id. Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet. Dignissim et risus amet, nisl, tortor quis ipsum est. Dui convallis eget nibh ac.
+              Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet.
+            </p>
+            <div className="textarea-input">
+              <textarea 
+                placeholder="Cole sua receita aqui"
+                name="textarea" 
+                id="textarea" 
+                cols="30" 
+                rows="10"
+                autoFocus
+                required
+              />
+              <button className="continuar">Continuar</button>
+            </div>
+          </div>
+        </main>
+      </div >
+      <footer className="footer">
+        <p>Copyrigth © 2020 - Porccionne</p>
       </footer>
-    </div >
+    </div>
   )
 }
