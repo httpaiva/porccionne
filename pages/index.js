984d4875885960073876c8e811057325a7469655
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-07-26 17:55:10 -0300

    Adicionando campos do formulário.

--- pages/index.js
+++ pages/index.js
@@ -24,24 +24,42 @@ export default function Home() {
           <h1 className="subtitulo">Produza suas receitas conosco</h1>
         </header>
         <main>
-          <div className="inicial">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque dui vitae amet tortor. Imperdiet auctor faucibus aliquam etiam eu in tellus sit id. Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet. Dignissim et risus amet, nisl, tortor quis ipsum est. Dui convallis eget nibh ac.
-              Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet.
+          <form id="form1" >
+            <div className="formulario1">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque dui vitae amet tortor. Imperdiet auctor faucibus aliquam etiam eu in tellus sit id. Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet. Dignissim et risus amet, nisl, tortor quis ipsum est. Dui convallis eget nibh ac.
+                Curabitur donec varius dolor fringilla morbi nisl. Quis pretium, pretium turpis lorem pretium fringilla massa. Sapien vel proin enim, adipiscing amet.
             </p>
-            <div className="textarea-input">
-              <textarea 
-                placeholder="Cole sua receita aqui"
-                name="textarea" 
-                id="textarea" 
-                cols="30" 
-                rows="10"
-                autoFocus
-                required
-              />
-              <button className="continuar">Continuar</button>
+              <div className="textarea-input">
+                <textarea
+                  placeholder="Cole sua receita aqui"
+                  name="textarea"
+                  id="textarea"
+                  cols="30"
+                  rows="10"
+                  autofocus
+                  required
+                />
+                <button className="continuar">Continuar</button>
+              </div>
             </div>
-          </div>
+          </form>
+          <form id="form2" >
+            <div className="formulario2" >
+              <div className="linha-receita">
+                <input type="number" name="quantidade" id="quantidade" min="1" max="100000" />
+                <input type="text" id="unidade" />
+                <input type="text" id="ingrediente" />
+              </div>
+              <div className="rendimento">
+                <label htmlFor="rende">Quanto a receita rende:</label>
+                <input type="number" name="rende" id="rende" min="1" max="1000" />
+                <label htmlFor="vairender">Quanto ela deve render:</label>
+                <input type="number" name="vairender" id="vairender" min="1" max="1000" />
+              </div>
+              <button className="porccionar"></button>
+            </div>
+          </form>
         </main>
       </div >
       <footer className="footer">
