32bc380778671840d38bda20288e37e484698a9b
Murilo Gomes <murilo.goomes@hotmail.com>
2020-08-06 16:47:17 -0300

    Fazendo com que a receita porcionada seja renderizada da maneita correta

--- components/resultado.js
+++ components/resultado.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Resultado(props) {
     return (
-        <div className="resultado-container">
+        <div className="resultado-container" style={{whiteSpace: "pre-wrap"}}>
             <h1>Sua receita porccionnada!</h1>
             <div className="resultado">
                 <p>{props.text}</p>
