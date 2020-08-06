c8192472a7fa5866e500d2f90c7e97cfef3fc384
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2020-08-06 13:39:43 -0300

    Criando funçao para separar os componetes da receita do texto
    Signed-off-by: eliseukadesh67 <eliseu.kadesh1967@gmail.com>
    Signed-off-by: murilogds <murilo.goomes@hotmail.com>
    Signed-off-by: gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
    
    Co-authored-by: eliseukadesh67 <eliseu.kadesh1967@gmail.com>
    Co-authored-by: murilogds <murilo.goomes@hotmail.com>
    Co-authored-by: gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>

---
+++ utils/SplitText.js
@@ -0,0 +1,12 @@
+function SplitText(text) {
+    let receitas = text.split('\n');
+    let resultado = []
+    for (var i = 0; i < receitas.length; i++) {
+        let f = {}
+        let word = receitas[i].split(/(\d+)/);
+        f.quantidade = word[1];
+        f.ingrediente = word[2];
+        resultado.push(f);
+    }
+    return resultado;
+}
