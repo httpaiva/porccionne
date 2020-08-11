506d96611348c56eaf74e4a720058869204fef3b
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2020-08-11 15:13:11 -0300

    Criando cards responsivos de creditos dos participantes do projeto.

--- pages/creditos.js
+++ pages/creditos.js
@@ -10,17 +10,74 @@ export default function Creditos() {
                     <h1 className="subtitulo">Conheça os criadores do Porccionne!</h1>
                 </header>
                 <main>
-                    <div className="card">
-                        <h2 className="nome">Jorge</h2>
-                        <p className="funcao">Programador</p>
+                    <div className="card-container">
+                        <div className="flipper">
+                            <div className="card front">
+                                <h2 className="nome">Matheus Gomes</h2>
+                                <div className="funcao">
+                                    <p>Idealizador</p>
+                                    <p>Desenvolvedor</p>
+                                </div>
+                            </div>
+                            <div className="card back">
+                                <p>Github</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card">
-                        <h2 className="nome">Jorge</h2>
-                        <p className="funcao">Programador</p>
+                    <div className="card-container">
+                        <div className="flipper">
+                            <div className="card front">
+                                <h2 className="nome">João Brum</h2>
+                                <div className="funcao">
+                                    <p>Designer</p>
+                                </div>
+                            </div>
+                            <div className="card back">
+                                <p>Behance</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card">
-                        <h2 className="nome">Jorge</h2>
-                        <p className="funcao">Programador</p>
+                    <div className="card-container">
+                        <div className="flipper">
+                            <div className="card front">
+                                <h2 className="nome">Gabriel Paiva</h2>
+                                <div className="funcao">
+                                    <p>Desenvolvedor</p>
+                                    <p>Front-end</p>
+                                </div>
+                            </div>
+                            <div className="card back">
+                                <p>Github</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container">
+                        <div className="flipper">
+                            <div className="card front">
+                                <h2 className="nome">Murilo Gomes</h2>
+                                <div className="funcao">
+                                    <p>Desenvolvedor</p>
+                                    <p>Back-end</p>
+                                </div>
+                            </div>
+                            <div className="card back">
+                                <p>Github</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-container">
+                        <div className="flipper">
+                            <div className="card front">
+                                <h2 className="nome">Eliseu Kadesh</h2>
+                                <div className="funcao">
+                                    <p>Desenvolvedor</p>
+                                    <p>Back-end</p>
+                                </div>
+                            </div>
+                            <div className="card back">
+                                <p>Github</p>
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
